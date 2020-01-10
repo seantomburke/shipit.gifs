2d5f9d9539b1f298d334b54ccf60124b73004a6f
Sean Thomas Burke <seantomburke@users.noreply.github.com>
2020-01-09 17:39:43 -0800

    fixing tests

--- cache.js
+++ cache.js
@@ -2,6 +2,6 @@
 let { gifs } = require('./gifs.json');
 const fs = require('fs');
 
-const names = gifs.reverse().map(gif => gif.name);
+const names = gifs.map(gif => gif.name);
 const cache = () => (_cache = { _names: names }, fs.writeFileSync('./.cache.json', JSON.stringify(_cache)), console.log('\033[38;5;229m', 'Cache updated'));
 cache();
--- test.js
+++ test.js
@@ -2,14 +2,16 @@ let { gifs } = require('./gifs.json');
 const { _names } = require('./.cache.json');
 const fs = require('fs');
 
-gifs = gifs.reverse();
 const red = '\033[38;5;124m𝗑\033[38;5;196m';
 const yellow = '\033[38;5;226m';
 const green = '\033[38;5;34m⚬\033[38;5;237m';
 const clear = '\033[2J';
-let errors = 1;
 let mark = green;
+let errors = 1;
 const names = gifs.map(gif => gif.name);
+const gifs_ = gifs.slice().reverse();
+console.log(gifs, gifs_);
+const names_ = names.slice().reverse();
 
 const assert = (message, condition) => ((mark = green) && condition && errors++ && (mark = red), console.log(mark, message));
 
@@ -19,22 +21,23 @@ const assert = (message, condition) => ((mark = green) && condition && errors++ 
  */
 const test = () => {
   console.log(clear);
-  assert('The existing names cannot be changed', _names.some((name, i) => gifs[i] && name !== gifs[i].name));
+  console.log('Running tests for shipit.gifs');
+  assert('The existing names cannot be changed', names_.some((name, i) => gifs_[i] && name !== gifs_[i].name));
   assert('The existing entries cannot be removed', gifs.length < _names.length);
-  assert('The _id is required', gifs.some(gif => !Number.isInteger(gif._id)));
-  assert('The _id needs to be the same as its index in the array', (gifs.reverse().some((gif, i) => gif._id !== i)));
-  assert('The _id needs to be a number', gifs.some(gif => !/\d+/g.test(gif._id) || !(typeof gif._id === 'number')));
-  assert('The name is required', gifs.some(gif => gif.name == null || !gif.name));
-  assert('The name must be less than 50 characters', gifs.some(gif => gif.name && gif.name.length > 50));
+  assert('The _id is required', gifs_.some(gif => !Number.isInteger(gif._id)));
+  assert('The _id needs to be the same as its index in the array', (gifs.some((gif, i) => gif._id !== i)));
+  assert('The _id needs to be a number', gifs_.some(gif => !/\d+/g.test(gif._id) || !(typeof gif._id === 'number')));
+  assert('The name is required', gifs_.some(gif => gif.name == null || !gif.name));
+  assert('The name must be less than 50 characters', gifs_.some(gif => gif.name && gif.name.length > 50));
   assert('The name must be unique', !(names.length === new Set(names).size));
   assert('The name must only contain lowercase letters, numbers and dashes (-)', gifs.some(gif => !/[a-z]+[a-z0-9-]*/g.test(gif.name)));
-  assert('The name must start with a lowercase letter', gifs.some(gif => !/^[a-z]/g.test(gif.name)));
-  assert('The url is required', gifs.some(gif => gif.url == null || !gif.url));
-  assert('The url must be less than 2000 characters', gifs.some(gif => gif.url && gif.url.length > 2000));
-  assert('The description cannot contain special characters', gifs.some(gif => !/[a-zA-Z0-9,.!&#@()'%$-_+=?]*/g.test(gif.name)));
-  assert('The description is required', gifs.some(gif => gif.description == null || !gif.description));
-  assert('The description must be less than 200 characters', gifs.some(gif => gif.description && gif.description.length > 200));
-  assert('The active state is required', gifs.some(gif => gif.active !== 1 && gif.active !== 0));
+  assert('The name must start with a lowercase letter', gifs_.some(gif => !/^[a-z]/g.test(gif.name)));
+  assert('The url is required', gifs_.some(gif => gif.url == null || !gif.url));
+  assert('The url must be less than 2000 characters', gifs_.some(gif => gif.url && gif.url.length > 2000));
+  assert('The description cannot contain special characters', gifs_.some(gif => !/[a-zA-Z0-9,.!&#@()'%$-_+=?]*/g.test(gif.name)));
+  assert('The description is required', gifs_.some(gif => gif.description == null || !gif.description));
+  assert('The description must be less than 200 characters', gifs_.some(gif => gif.description && gif.description.length > 200));
+  assert('The active state is required', gifs_.some(gif => gif.active !== 1 && gif.active !== 0));
 }
 
 test(); 
