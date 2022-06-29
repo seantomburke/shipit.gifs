0a17769d475c6d91ccb1d0544b52e6fcc0d87692
Riley Hilliard <akuztikbeatz@gmail.com>
2022-06-29 11:20:07 -0500

    narrow diff

--- scripts/pull-request.js
+++ scripts/pull-request.js
@@ -3,7 +3,7 @@ const exec = util.promisify(require('child_process').exec);
 
 async function run() {
   const r = new RegExp(/(\+      "url": "|",\n)/gm);
-  const { stdout: rawGIFLines } = await exec(`git diff master... | grep '+      "url": '`);
+  const { stdout: rawGIFLines } = await exec(`git diff master... gifs.json | grep '+      "url": '`);
   const newGIFLinks = rawGIFLines.replace(r, '').split('+      url: ');
   const formattedGIFMarkDown = newGIFLinks.map((gifURL) => `![New GIF](${gifURL})\n`);
   const pr = await exec(`gh pr create --title 'Adding More Gifs' --body '## Added GIFs \n\n ${formattedGIFMarkDown}'`);
