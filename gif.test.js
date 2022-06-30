7a5d475f0236b7ba21f976b7aaca6f80762e8a76
Riley Hilliard <akuztikbeatz@gmail.com>
2022-06-30 14:50:31 -0500

    re-write tests

--- gif.test.js
+++ gif.test.js
@@ -10,109 +10,91 @@ const START_WITH_LOWER_CASE_REGEX = new RegExp(/^[a-z]/);
 const MAX_SIZE_IN_BYTES = 5000000;
 
 function isValidHttpUrl(string) {
-  let url;
+  let protocol;
 
   try {
-    url = new URL(string);
+    protocol = new URL(string)?.protocol;
   } catch (_) {
     return false;
   }
 
-  return url.protocol === 'http:' || url.protocol === 'https:';
+  return protocol === 'http:' || protocol === 'https:';
 }
 
-describe('Validating all GIFs', () => {
-  let allNames = new Set();
-  let existingURLs = new Set();
+let allNames = new Set();
+let existingURLs = new Set();
 
-  gifs.forEach(({ _id, name, url, description, active }, i) => {
-    allNames.add(name);
+gifs.forEach(({ _id, name, url, description, active }, i) => {
+  allNames.add(name);
 
-    // Only pre-existing GIFs are tested here
-    if (_id < existingGifs.length) {
-      test(`${name}: Existing GIFs should not be modified`, () => {
-        // The existing names cannot be changed
-        expect(name).toBe(existingGifs[i]);
-      });
+  describe(`Validating entry: '${name}'`, () => {
+    // Only newly added GIFs are tested here
+    if (_id > existingGifs.length - 1) {
+      const { host, search, hash } = urlUtil.parse(url);
 
-      existingURLs.add(url);
+      test('GIF URL doesnt already exist', () => expect(existingURLs.has(url)).not.toBe(true));
 
-      // Only newly added GIFs are tested here
-    } else {
-      test(`${name}: New GIF URL doesnt already exist`, () => {
-        expect(existingURLs.has(url)).not.toBe(true);
-      });
-
-      test(`${name}: New GIF size is less than 5MB`, async () => {
+      test('GIF size is less than 5MB', async () => {
         const { length: bytes } = await probe(url);
         expect(bytes).toBeLessThan(MAX_SIZE_IN_BYTES);
       });
+
+      test('The _id needs to be the same as its index in the array', async () => expect(_id).toBe(i));
+
+      test('The _id needs to be a number', async () => {
+        expect(IS_NUMBER_REGEX.test(_id)).toBe(true);
+        expect(typeof _id).toBe('number');
+        expect(Number.isInteger(_id)).toBe(true);
+      });
+
+      test('New names must be unique', async () => expect(existingGifs.includes(name)).not.toBe(true));
+
+      test('The name is required', async () => expect(typeof name).toBe('string'));
+
+      test('The name must be less than 50 characters', async () => expect(name.length).toBeLessThan(50));
+
+      test('The name must be greater than 1 character', async () => expect(name.length).toBeGreaterThan(1));
+
+      test('The name must only contain lowercase letters, numbers and dashes (-)', async () =>
+        expect(LOWER_CASE_REGEX.test(name)).toBe(true));
+
+      test('The name must start with a lowercase letter', async () =>
+        expect(START_WITH_LOWER_CASE_REGEX.test(name)).toBe(true));
+
+      test('The url is required', async () => expect(typeof url).toBe('string'));
+
+      test('The url must be less than 2000 characters', async () => expect(url.length).toBeLessThan(2000));
+
+      test('the URL is a valid URL with a protocol', async () => expect(isValidHttpUrl(url)).toBe(true));
+
+      test('The url domain must be an approved domain', async () =>
+        expect(domains.includes(host.substring(host.indexOf('.') + 1))).toBe(true));
+
+      test('The url must not have query params', async () => expect(!!search).not.toBe(true));
+
+      test('The url must not have a fragment', async () => expect(!!hash).not.toBe(true));
+
+      test('The description is required', async () => expect(typeof description).toBe('string'));
+
+      test('The description cannot contain special characters', async () =>
+        expect(SPECIAL_CHAR_REGEX.test(description)).toBe(true));
+
+      test('The description must be less than 200 characters', async () =>
+        expect(description.length).toBeLessThan(200));
+
+      test('The active state is required', async () => {
+        expect(active).toBeGreaterThan(-1);
+        expect(active).toBeLessThan(2);
+      });
+
+      // Only pre-existing GIFs are tested here
+    } else {
+      existingURLs.add(url);
+
+      test('Existing GIFs should not be modified or removed', () => {
+        expect(name).toBe(existingGifs[i]);
+        expect(allNames.has(name)).toBe(true);
+      });
     }
-
-    // Every GIF (new or old) is tested below here
-    test(`${name}: GIF should be valid`, () => {
-      // The _id needs to be the same as its index in the array
-      expect(_id).toBe(i);
-
-      // The _id needs to be a number
-      expect(IS_NUMBER_REGEX.test(_id)).toBe(true);
-      expect(typeof _id).toBe('number');
-      expect(Number.isInteger(_id)).toBe(true);
-
-      // New names must be unique
-      expect(name).not.toBe(existingGifs.includes(name));
-
-      // The name is required
-      expect(typeof name).toBe('string');
-
-      // The name must be less than 50 characters
-      expect(name.length).toBeLessThan(50);
-
-      // The name must be greater than 1 character
-      expect(name.length).toBeGreaterThan(1);
-
-      // The name must only contain lowercase letters, numbers and dashes (-)
-      expect(LOWER_CASE_REGEX.test(name)).toBe(true);
-
-      // The name must start with a lowercase letter
-      expect(START_WITH_LOWER_CASE_REGEX.test(name)).toBe(true);
-
-      // The url is required
-      expect(typeof url).toBe('string');
-
-      // The url must be less than 2000 characters
-      expect(url.length).toBeLessThan(2000);
-
-      // the URL is a valid URL with a protocol
-      expect(isValidHttpUrl(url)).toBe(true);
-
-      // The url domain must be an approved domain
-      const { host } = urlUtil.parse(url);
-      expect(domains.includes(host.substring(host.indexOf('.') + 1))).toBe(true);
-
-      // The url must not have query params
-      expect(!!urlUtil.parse(url).search).not.toBe(true);
-
-      // The url must not have a fragment
-      expect(!!urlUtil.parse(url).hash).not.toBe(true);
-
-      // The description is required
-      expect(typeof description).toBe('string');
-
-      // The description cannot contain special characters
-      expect(SPECIAL_CHAR_REGEX.test(description)).toBe(true);
-
-      // The description must be less than 200 characters
-      expect(description.length).toBeLessThan(200);
-
-      // The active state is required
-      expect(active).toBeGreaterThan(-1);
-      expect(active).toBeLessThan(2);
-    });
-  });
-
-  test('The existing entries cannot be removed', () => {
-    const existingGIFsStillExist = existingGifs.every((name, i) => allNames.has(name));
-    expect(existingGIFsStillExist).toBe(true);
   });
 });
